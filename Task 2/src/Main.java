3a9cb1816b60bd1953b5c8e64a045f2b104cfe80
atomic <ilyaklimof@gmail.com>
2025-04-13 16:35:26 +0300

    version 0.1

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+
+        /*for (int i = 1; i <= 100 ; i++){
+            if (i % 2 == 0) {
+                System.out.println("Уволен айтишник с id - " + i);
+            }
+        }*/
+        int count = 0;
+        int i = 1;
+        while ( i <= 100 ) {
+            i++;
+            count++;
+            while ( i <=100 && i % 2 == 0 ) {
+                i++;
+                count++;
+                System.out.println("Уволен айтишник с id - " + count);
+            }
+        }
     }
 }
